09e12207fca0522be804d4d7d7531394981083ca
Stefan Sarow <stefan@pixelaffairs.com>
2020-09-24 21:42:28 +0200

    removed console.log

--- index.js
+++ index.js
@@ -7,6 +7,4 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+app.listen(PORT);
