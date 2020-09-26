38cfb2efc301f343bbe968ea5a39acc3e4acdd6b
Stefan Sarow <stefan@pixelaffairs.com>
2020-09-26 19:33:36 +0200

    added proxy for heroku

--- services/passport.js
+++ services/passport.js
@@ -21,6 +21,7 @@ passport.use(
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
       callbackURL: "/auth/google/callback",
+      proxy: true, // only use when you trust the server (here Heroku - I trust)
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id })
