0327a4f11a19c291e50eaa3fe8b26a2ceb8d858b
Stefan Sarow <stefan@pixelaffairs.com>
2020-10-12 20:43:00 +0200

    made production ready for heroku

--- index.js
+++ index.js
@@ -31,6 +31,17 @@ app.use(passport.session());
 authRoutes(app);
 billingRoutes(app);
 
+if (process.env.NODE_ENV === "production") {
+  // express serves assets in production (like main.js)
+  app.use(express.static("client/build"));
+
+  // express serves index.html if route is unknown
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
