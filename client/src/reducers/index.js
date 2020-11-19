4039f07d8595cc77b66ab5657800f06b82e28a37
Stefan Sarow <stefan@pixelaffairs.com>
2020-11-19 11:51:43 +0100

    working prototyp

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from "redux";
 import { reducer as ReduxForm } from "redux-form";
 
 import authReducer from "./authReducer";
+import surveysReducer from "./surveysReducer";
 
 export default combineReducers({
   auth: authReducer,
   form: ReduxForm,
+  surveys: surveysReducer,
 });
